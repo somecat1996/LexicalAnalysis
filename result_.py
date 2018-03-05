4588295411c98d1d08fc7bde7a4794f7e6edf0e1
somecat1996 <369150573@qq.com>
2018-03-05 17:44:11 +0800

    curriculum design
    1. Converting the non decimal number in the code to a decimal number.
    2. Convert uppercase letters in comments to lowercase letters.

---
+++ result_.py
@@ -0,0 +1,71 @@
+26 161 26 161
+15 15  15
+2 2
+
+_33
+a0x21
+_skdasd0x123asdkjh
+
+
+''' abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017  '''
+
+''' abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017
+'''
+
+'''
+abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017 '''
+
+'''
+abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017
+'''
+
+""" abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017 """
+
+""" abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017
+"""
+
+"""
+abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017 """
+
+"""
+abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017
+"""
+
+""" '''
+abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017
+'''
+abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017
+"""
+
+""" '''
+abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017
+'''
+abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017
+'''
+"""
+
+""" # abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017
+''' """
+
+# abcdefghijklmnopqrstu
+
+# ''' """ abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017 """ abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017 ''' abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017 ''' '''
+
+# """ """ abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017 """ abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017 ''' abcdefghijklmnopqrstu 0x1a 0xa1 0o17 017 ''' """
+
+' ABCDEFGhijklmnOPQRSTU 0x1A 0xA1 0o17 017 '
+
+" ABCDEFGhijklmnOPQRSTU 0x1A 0xA1 0o17 017 "
+
+' \
+ABCDEFGhijklmnOPQRSTU 0x1A 0xA1 0o17 017 '
+
+" \
+ABCDEFGhijklmnOPQRSTU 0x1A 0xA1 0o17 017 "
+
+' # ABCDEFGhijklmnOPQRSTU 0x1A 0xA1 0o17 017 \
+ABCDEFGhijklmnOPQRSTU 0x1A 0xA1 0o17 017 '
+
+" # ABCDEFGhijklmnOPQRSTU 0x1A 0xA1 0o17 017 \
+ABCDEFGhijklmnOPQRSTU 0x1A 0xA1 0o17 017 \
+"
